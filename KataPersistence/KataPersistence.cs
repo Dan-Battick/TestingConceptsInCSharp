cee49e67bf253ca3b5c6c1434381ae80029b991c
Daniel Battick <danielbattick@gmail.com>
2021-08-23 16:55:17 -0500

    Finished KataPersistence

--- KataPersistence/KataPersistence.cs
+++ KataPersistence/KataPersistence.cs
@@ -10,15 +10,32 @@ namespace KataPersistence
     {
         static void Main(string[] args)
         {
-            long num = 2425693232143635;
-            //Console.WriteLine(CountDigits(num));
-            Console.WriteLine(MultiplyDigits(num));
+            long num = 999;
+            //Console.WriteLine($"The number of digits in { num } = { CountDigits(num) }");
+            //Console.WriteLine($"The multiplication of all digits = { MultiplyDigits(num) }");
+            Console.WriteLine($"The persistence of {num} is {Persistence(num)}");
         }
 
-        public static int CountDigits(long num)
+        public static int Persistence(long n)
         {
+            int counter = 0;
+            int digits = countDigits(n);
+            while (digits > 1)
+            {
+                counter += 1;
+                long mult = multiplyDigits(n);
+                //Console.WriteLine($"The value of mult now is {mult}");
+                n = mult;
+                digits = countDigits(mult);
+            }
+            return counter;
+        }
+
+        private static int countDigits(long num)
+        {
+            /*
             // This loop can count for all integers, but takes a longer time
-            /*int count = 0;
+            int count = 0;
             while (num != 0)
             {
                 num /= 10;
@@ -26,19 +43,19 @@ namespace KataPersistence
             }
             return count;*/
 
-            return Convert.ToInt32(Math.Floor(Math.Log10(num) + 1)); // Does not work for 0 or negative numbers.
+            return (int)Math.Floor(Math.Log10(num) + 1); // Does not work for 0 or negative numbers.
         }
 
-        public static int MultiplyDigits(long n)
+        private static long multiplyDigits(long n)
         {
-            int mult = 1;
-            while (CountDigits(n) > 1)
+            long mult = 1;
+            while (countDigits(n) > 1)
             {
-                int lastDigit = Convert.ToInt32(n % 10);
+                long lastDigit = n % 10;
                 mult *= lastDigit;
                 n /= 10;
             }
-            return mult * (int) n;
+            return (long)(mult * n);
         }
     }
 }
