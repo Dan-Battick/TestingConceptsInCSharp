57f9fea71c4ecd2ef9678fa7f10dbefea753a2dd
Daniel Battick <danielbattick@gmail.com>
2021-08-23 14:05:22 -0500

    Added 2 functions: CountDigits and MultiplyDigits

--- KataPersistence/KataPersistence.cs
+++ KataPersistence/KataPersistence.cs
@@ -2,28 +2,43 @@
 
 namespace KataPersistence
 {
+    /// <summary>
+    /// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, 
+    /// which is the number of times you must multiply the digits in num until you reach a single digit.
+    /// </summary>
     class KataPersistence
     {
         static void Main(string[] args)
         {
-            int num = 635698;
-            int count = 0;
-            int mult = 1;
-            /*while (num != 0)
-            {
-                count++;
-                int lastDigit = num % 10;
-                mult *= lastDigit; 
-                num = num / 10;
-            }*/
-            Console.WriteLine(Math.Floor(Math.Log10(num) + 1));
-            //Console.WriteLine(mult);
+            long num = 2425693232143635;
+            //Console.WriteLine(CountDigits(num));
+            Console.WriteLine(MultiplyDigits(num));
         }
 
-        public static int MultiplyDigits(int n)
+        public static int CountDigits(long num)
         {
+            // This loop can count for all integers, but takes a longer time
+            /*int count = 0;
+            while (num != 0)
+            {
+                num /= 10;
+                ++count;
+            }
+            return count;*/
 
-            return -1;
+            return Convert.ToInt32(Math.Floor(Math.Log10(num) + 1)); // Does not work for 0 or negative numbers.
+        }
+
+        public static int MultiplyDigits(long n)
+        {
+            int mult = 1;
+            while (CountDigits(n) > 1)
+            {
+                int lastDigit = Convert.ToInt32(n % 10);
+                mult *= lastDigit;
+                n /= 10;
+            }
+            return mult * (int) n;
         }
     }
 }
